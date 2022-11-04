cb0998658fe03e3235ee4ae1fa162c21acfd93d7
paulavivas <paulavivasteixeira@gmail.com>
2022-11-03 23:27:53 -0400

    new styling(event's card and h1:: afte)r

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
       <footer className="appFooter wrapper">
         <p>
           Made by
-          <a href="https://www.paulavivas.ca/"> Paula Vivas Teixeira</a> at <a href="https://junocollege.com/">Juno College te Technology</a>
+          <a href="https://www.paulavivas.ca/"> Paula Vivas Teixeira</a> at <a href="https://junocollege.com/">Juno College of Technology</a>
         </p>
         <p>This page was created using React and <a href="https://www.ticketmaster.ca/"> Ticketmaster API</a>. Visit my <a href="https://github.com/paulavivasteixeira">Github page</a> to view the code.</p>
       </footer>
